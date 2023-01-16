bded375020e89263e97648e9f1df6e2876b32676
ccllaarraa <clarapasteau@gmail.com>
2023-01-16 11:36:41 +0100

    update watch

--- script2022.js
+++ script2022.js
@@ -32,4 +32,28 @@ $(document).ready(function() {
 })
 
 
-// lazy load
+
+// clock js
+var inc = 1000;
+
+clock();
+
+function clock() {
+    var date = new Date();
+
+  var hours = ((date.getHours() + 11) % 12 + 1);
+  var minutes = date.getMinutes();
+  var seconds = date.getSeconds();
+  
+  var hour = hours * 30;
+  var minute = minutes * 6;
+  var second = seconds * 6;
+  
+  var classhour = document.getElementById('hour');
+
+  classhour.style.transform = "rotate(" + hour + "deg)";
+  document.querySelector('.minute').style.transform =  "rotate(" + minute + "deg)";
+  document.querySelector('.second').style.transform =  "rotate(" + second + "deg)";
+}
+
+setInterval(clock, inc);
